8cf94dffa06b9d7149ea170b527f25e70bdc1697
tizorios <riostiziano6@gmail.com>
2023-07-28 16:19:28 -0300

    actualizado

---
+++ Server/src/routes/index.js
@@ -0,0 +1,20 @@
+const { getCharById } = require('../controllers/getCharById');
+const login = require('../controllers/login')
+const postUser = require('../controllers/postUser')
+const postFav = require('../controllers/postFavs')
+const deleteFav = require('../controllers/deleteFav')
+
+
+const router = require('express').Router();
+
+router.get('/character/:id', login);
+
+router.get('/login', postUser);
+
+router.post('/fav', postFav);
+
+router.delete('/fav/:id', deleteFav);
+
+router.get('/character/:id', getCharById);
+
+module.exports = router;
